de7fb97605ee9c9e092ae0dd9206adb941e194e0
nazirumar <nazirumar888@gmail.com>
2022-06-12 22:11:35 +0100

    snake

--- Snake.py
+++ Snake.py
@@ -112,7 +112,56 @@ def main():
         font =pygame.font.SysFont('arial', 64)
         text_surface =font.render('NAKER', True, BLUE)
         screen.blit(text_surface, (220,180))
-        font=pygame.font.SysFont
+        font=pygame.font.SysFont("arial", 24)
+        text_surface =font.render("Move the snake with arrow Keys  to eat the apples", True, BLUE)
+        screen.blit(text_surface, (50,300))
+        text_surface =font.render("Avoid The Walls and Yourself !", True, BLUE)
+        screen.blit(text_surface, (50,350))
+        text_surface = font.render("Press s to start a new game - Press q to quit at any time", True, BLUE)
+        screen.blit(text_surface, (50,400))
+        text_surface = font.render("Press p to pause r to resume at any time", True, BLUE)
+        screen.blit(text_surface, (50,450))
+        pygame.display.flip()
+        while 1:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    exit()
+
+                pressed_keys = pygame.key.get_pressed()
+                if pressed_keys[K_q]: exit()
+                if pressed_keys[K_s]: break
+
+                clock.tick(10)
+
+        while not snakedead:
+
+            ###### get input events  ####
+
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    exit()
+            pressed_keys = pygame.key.get_pressed()
+
+
+            if pressed_keys[K_LEFT]: newdirection = LEFT
+            if pressed_keys[K_RIGHT]: newdirection = RIGHT
+            if pressed_keys[K_UP]: newdirection = UP
+            if pressed_keys[K_DOWN]: newdirection = DOWN
+            if pressed_keys[K_q]: snakedead = TRUE
+            if pressed_keys[K_p]: gamepaused = 1
+
+
+         ### wait here if p key is pressed until p key is pressed again
+            
+            while gamepaused == 1:
+                for event in pygame.event.get():
+                    if event.type == QUIT:
+                        exit()
+                pressed_keys = pygame.key.get_pressed()
+                if pressed_keys[K_r]:
+                    gamepaused = 0 
+                clock.tick(10)
+
 
 
 
